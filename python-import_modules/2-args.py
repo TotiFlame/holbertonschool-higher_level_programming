66f0023eb496b943664599305a30358812f8b605
TotiFlame <santifer4321@gmail.com>
2022-09-09 11:04:19 -0300

    Task 2 Second attempt

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     len = len(sys.argv)
     if len == 1:
         print("0 arguments.")
+    elif len == 2:
+        print("{} argument:".format(len - 1))
+        print("{}: {}".format(1, sys.argv[1]))
     else:
         print("{} arguments:".format(len - 1))
         for i in range(1, len):
