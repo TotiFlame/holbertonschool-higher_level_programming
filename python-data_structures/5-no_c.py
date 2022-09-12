b7f6c4762de32e3b15ca4129468e0aae8a034c2e
TotiFlame <santifer4321@gmail.com>
2022-09-12 18:22:13 -0300

    Task 5 Second attempt

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+from xxlimited import new
+
+
 def no_c(my_string):
-    my_string = my_string.replace("C", "")
-    my_string = my_string.replace("c", "")
-    return my_string
+    new_string = ''
+    for i in my_string:
+        if i == 'c' or i == 'C':
+            i = ''
+        new_string += i
+    return new_string
