29a61d7b3e39b59bd782dba5ca299410dfe2fb65
TotiFlame <santifer4321@gmail.com>
2022-09-13 15:24:36 -0300

    Task 10 Second attempt

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     new_list = [None] * len(my_list)
     for i in range(0, len(my_list)):
-        if (i % 2) == 0:
+        if (my_list[i] % 2) == 0:
             new_list[i] = True
         else:
             new_list[i] = False
