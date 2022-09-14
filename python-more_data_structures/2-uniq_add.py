4098e4dd563950e502f2030d49253d4ebc74d7fd
TotiFlame <santifer4321@gmail.com>
2022-09-14 17:31:04 -0300

    Task 2 First attempt

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+def uniq_add(my_list=[]):
+    new_list = []
+    res = 0
+    for i in my_list:
+        if i not in new_list:
+            new_list.append(i)
+    for j in new_list:
+        res += j
+    return res
