ddac26c4dedf379387380f2471c6e2371f13ef24
TotiFlame <santifer4321@gmail.com>
2022-09-14 18:04:59 -0300

    Task 3 Second attempt

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for f_set in set_1:
-        for s_set in set_2:
-            if f_set == s_set:
-                return f_set
+    intersection = set_1.intersection(set_2)
+    set_int = list(intersection)
+    return set_int
