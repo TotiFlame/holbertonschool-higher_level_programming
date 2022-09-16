656992c48c1270e7acdd7338065ea3d09bb2d9ec
TotiFlame <santifer4321@gmail.com>
2022-09-16 13:12:18 -0300

    Task 12 First attempt

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    res = 0
+    char = 0
+    while char < len(roman_string):
+        first_num = sum(roman_string[char])
+        if (char + 1) < len(roman_string):
+            second_num = sum(roman_string[char + 1])
+            if first_num < second_num:
+                res += (second_num) - (first_num)
+                char += 2
+            else:
+                res += first_num
+                char += 1
+        else:
+            res += first_num
+            char += 1
+    return res
+
+
+def sum(number):
+    if number == 'I':
+        return 1
+    if number == 'V':
+        return 5
+    if number == 'X':
+        return 10
+    if number == 'L':
+        return 50
+    if number == 'C':
+        return 100
+    if number == 'D':
+        return 500
+    if number == 'M':
+        return 1000
+    return -1
