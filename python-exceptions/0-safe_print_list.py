c422354ddec7b203724550e1c480cfcc1fa9f612
TotiFlame <santifer4321@gmail.com>
2022-09-16 14:15:23 -0300

    Task 0 Second attempt

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     if my_list is None:
-        return None
+        return 0
+    if x == 0:
+        return 0
     try:
         for i in range(0, x):
             print(f"{my_list[i]}", end="")
