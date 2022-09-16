fde5598f2e4892b9ba93ad56cbb9df4f16fb345d
TotiFlame <santifer4321@gmail.com>
2022-09-16 14:14:22 -0300

    Task 0 First attempt

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,11 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     if my_list is None:
         return None
-    if x > len(my_list):
-        ite = len(my_list)
-    else:
-        ite = x
-    for i in range(0, ite):
-        print(f"{my_list[i]}", end="")
-    print()
+    try:
+        for i in range(0, x):
+            print(f"{my_list[i]}", end="")
+        print()
+    except Exception:
+        print()
+        return i
     return i + 1
