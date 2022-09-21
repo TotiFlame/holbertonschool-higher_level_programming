bb5cf8055f48a0a6f60b4a162f094b927574fed6
TotiFlame <santifer4321@gmail.com>
2022-09-21 14:42:39 -0300

    Tests tasks Task 0 First attempt

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
+    if type(a) is not int:
+        raise TypeError("a must be an integer")
+    if type(b) is not int:
+        raise TypeError("b must be an integer")
+    return (a + b)
---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,18 @@
+def matrix_divided(matrix, div):
+    new_matrix = []
+    if type(div) != int and type(div) != float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    length = len(matrix[0])
+    for idx, row in enumerate(matrix):
+        if len(row) != length:
+            raise TypeError("Each row of the matrix "
+                                "must have the same size")
+        new_matrix.append([])
+        for j in row:
+            if type(j) != int and type(j) != float:
+                raise TypeError("matrix must be a matrix"
+                                "(list of lists) of integers/floats")
+            new_matrix[idx].append(round(j / div, 2))
+    return new_matrix
