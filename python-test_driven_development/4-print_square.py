f50adcc27e7fb9ce43a04a18208c0733dd4da724
TotiFlame <santifer4321@gmail.com>
2022-09-21 16:51:52 -0300

    Task 4 and Test 4 First attempt

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" module """
+
+
+def print_square(size):
+    """ print_square """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    for i in range(size):
+        if i != 0:
+            print("")
+        for j in range(size):
+            print("#", end="")
+    print()
