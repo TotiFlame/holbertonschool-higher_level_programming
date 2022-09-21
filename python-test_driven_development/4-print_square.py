b65e0ae92d022ac14b2dfcca2030ce43004873b0
TotiFlame <santifer4321@gmail.com>
2022-09-21 16:54:07 -0300

    Task 4 Second attempt

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -10,6 +10,9 @@ def print_square(size):
         raise ValueError("size must be >= 0")
     if type(size) is float and size < 0:
         raise TypeError("size must be an integer")
+    if size == 0:
+        print()
+        return
     for i in range(size):
         if i != 0:
             print("")
