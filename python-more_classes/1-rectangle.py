5e9e048f6b1c876ee9268435080d4e9e5cf879c0
TotiFlame <santifer4321@gmail.com>
2022-09-22 13:35:50 -0300

    Task 1 First attempt

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" module """
+
+
+class Rectangle:
+    """ Rectangle """
+    def __init__(self, width=0, height=0):
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def height(self, value):
+        self.__height = value
