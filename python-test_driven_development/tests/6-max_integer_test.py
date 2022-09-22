3c3f1b05ed20aab6dac6090bf330ca6d78029fe9
TotiFlame <santifer4321@gmail.com>
2022-09-22 11:07:46 -0300

    Task 6 First attempt

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    def test_messy_list(self):
+        result = max_integer([10, 8, 23, 18, 5])
+        self.assertEqual(result, 23)
+
+    def test_sorted_list(self):
+        result = max_integer([9, 11, 23, 45, 62])
+        self.assertEqual(result, 62)
+
+    def test_empty_list(self):
+        result = max_integer([])
+        self.assertEqual(result, None)
+
+    def test_letter_list(self):
+        result = max_integer(["hola", "chau"])
+        self.assertEqual(result, "hola")
+
+    if __name__ == '__main__':
+        unittest.main()
