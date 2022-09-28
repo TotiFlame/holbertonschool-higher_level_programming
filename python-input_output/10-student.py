84988fb3b7eadd1439a405733fa2e34547496ce7
TotiFlame <santifer4321@gmail.com>
2022-09-28 15:53:12 -0300

    Task 10 First attempt

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" students """
+
+
+class Student():
+    """ Module """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            new_dict = {}
+            for atr in attrs:
+                for key, value in self.__dict__.items():
+                    if atr == key:
+                        new_dict[key] = value
+            return new_dict
+        else:
+            return self.__dict__
