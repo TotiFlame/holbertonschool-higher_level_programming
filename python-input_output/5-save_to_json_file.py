5cbf9ecf54efc1fd9e85d6ba6c10640b848660af
TotiFlame <santifer4321@gmail.com>
2022-09-28 11:56:06 -0300

    Task 5 Second attempt

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,7 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """ Module """
-    jsonStr = json.dumps(my_obj)
     with open(filename, "w") as f:
-        f.write(jsonStr)
-    f.closed
+        f.write(json.dumps(my_obj))
