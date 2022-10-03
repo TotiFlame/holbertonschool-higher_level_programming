d2a44949a7f7bca643fac7a22c916cab6f401779
TotiFlame <santifer4321@gmail.com>
2022-10-03 14:05:34 -0300

    Task 12 First attempt

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -28,3 +28,28 @@ class Square(Rectangle):
         if value <= 0:
             raise ValueError("height must be > 0")
         self.height = value
+
+    def update(self, *args, **kwargs):
+        if len(args) != 0:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[0]
+                elif i == 1:
+                    self.size = args[1]
+                elif i == 2:
+                    self.x = args[2]
+                elif i == 3:
+                    self.y = args[3]
+                else:
+                    return
+        for key, value in kwargs.items():
+            if key == "id":
+                self.id = value
+            elif key == "size":
+                self.size = value
+            elif key == "x":
+                self.x = value
+            elif key == "y":
+                self.y = value
+            else:
+                return
