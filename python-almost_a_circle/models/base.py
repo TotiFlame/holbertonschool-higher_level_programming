c12c787cf88eb6aae085a1b7c655d5b423bef07a
TotiFlame <santifer4321@gmail.com>
2022-10-03 14:52:59 -0300

    Task 15 json function documented

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,6 +16,7 @@ class Base():
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
+        """ json list """
         if list_dictionaries is None:
             return "[]"
         x = json.dumps(list_dictionaries)
