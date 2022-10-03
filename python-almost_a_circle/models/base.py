362c8e9c0c31e1dd0994db942f34b3b868d07677
TotiFlame <santifer4321@gmail.com>
2022-10-03 17:16:09 -0300

    Task 16 First attempt

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,3 +22,16 @@ class Base():
             return "[]"
         x = json.dumps(list_dictionaries)
         return x
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        #print(cls.__name__ + ".json" + list_objs)
+        if list_objs is None:
+            with open(cls.__name__ + ".json", "w") as f:
+                f.write("[]")
+            return
+        with open(cls.__name__ + ".json", "w") as f:
+            json_str = ""
+            for item in list_objs:
+                json_str += cls.to_json_string(item.to_dictionary())
+            f.write(json_str)
