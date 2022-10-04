06ead09330020b49571847fcb59c74f0133e23ab
TotiFlame <santifer4321@gmail.com>
2022-10-04 13:43:54 -0300

    Task 16 Fist attempt

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ base """
 import json
+import os
 
 
 class Base():
-    """ Base class """
-
+    # Base class
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -18,20 +18,23 @@ class Base():
     @staticmethod
     def to_json_string(list_dictionaries):
         """ json list """
-        if list_dictionaries is None:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         x = json.dumps(list_dictionaries)
         return x
 
     @classmethod
     def save_to_file(cls, list_objs):
-        #print(cls.__name__ + ".json" + list_objs)
+        file_name = cls.__name__ + ".json"
         if list_objs is None:
-            with open(cls.__name__ + ".json", "w") as f:
+            with open(file_name, "w") as f:
                 f.write("[]")
             return
-        with open(cls.__name__ + ".json", "w") as f:
-            json_str = ""
-            for item in list_objs:
-                json_str += cls.to_json_string(item.to_dictionary())
+        json_str = "["
+        for num, item in enumerate(list_objs):
+            if num != 0:
+                json_str += ", "
+            json_str += (cls.to_json_string(item.to_dictionary()))
+        json_str += "]"
+        with open(file_name, "w") as f:
             f.write(json_str)
