8edb5dcbb6efb8791928e0130c240da41d4aafb4
TotiFlame <santifer4321@gmail.com>
2022-11-04 15:09:20 -0300

    Task 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS second_table (
     id int,
     name varchar(256),
     score int
-)
+);
 
 INSERT INTO second_table (id, name, score) VALUES(1, "John", 10)
 INSERT INTO second_table (id, name, score) VALUES(2, "Alex", 3)
