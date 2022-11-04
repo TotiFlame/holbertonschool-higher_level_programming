300ab7c42b8b12ff22539578574d1b489699a6c6
TotiFlame <santifer4321@gmail.com>
2022-11-04 16:14:24 -0300

    Task 14

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,3 +1,7 @@
 -- compute the score average of all records in the table second_table
 -- task 14
-SELECT AVG(score) FROM second_table
+
+ALTER TABLE second_table
+ADD average float;
+
+INSERT INTO second_table (average) SELECT AVG(score) FROM second_table
