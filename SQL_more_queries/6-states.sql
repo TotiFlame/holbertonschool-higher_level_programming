8674733fa900ca655adf04b88b6d66525e066984
TotiFlame <santifer4321@gmail.com>
2022-11-05 15:41:37 -0300

    Task 5

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -2,6 +2,8 @@
 -- Task 6
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+USE hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS states (
     id int NOT NULL AUTO_INCREMENT UNIQUE,
     name varchar(256) NOT NULL,
