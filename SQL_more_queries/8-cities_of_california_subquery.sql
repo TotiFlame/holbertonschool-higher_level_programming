52a51c6d70eccd0c40230f3da87fef3d01b156ec
TotiFlame <santifer4321@gmail.com>
2022-11-05 16:05:06 -0300

    Task 7

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,3 @@
 -- list all the cities of California that can be found in the database
 -- TASK 8
-USE hbtn_0d_usa;
-
-SELECT id, name FROM cities WHERE state_id = 1 ORDER BY id ASC
+SELECT id, name FROM cities WHERE state_id = 1
