45e5c10ce7a904e7dc71e4f5068f79d1bacb40e5
TotiFlame <santifer4321@gmail.com>
2022-11-05 15:35:05 -0300

    Task 6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- Create the database hbtn_0d_usa and the table states
+-- Task 6
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+    id int NOT NULL AUTO_INCREMENT UNIQUE,
+    name varchar(256) NOT NULL,
+    PRIMARY KEY (id)
+);
