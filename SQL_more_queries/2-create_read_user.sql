287bc9f916b91fa98404a8b0a7ee865e33759a63
TotiFlame <santifer4321@gmail.com>
2022-11-05 15:13:54 -0300

    Task 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,4 +2,4 @@
 -- task 2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS "user_0d_2"@"localhost" IDENTIFIED BY "user_0d_2_pwd";
-GRANT PRIVILEGES SELECT ON * . * TO 'user_0d_2'@'localhost';
+GRANT PRIVILEGES SELECT ON hbtn_0d_2 . * TO 'user_0d_2'@'localhost';
