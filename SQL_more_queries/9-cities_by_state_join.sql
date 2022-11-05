9479f247158a093683f19f011d97ed6c61faeda9
TotiFlame <santifer4321@gmail.com>
2022-11-05 16:24:07 -0300

    Task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- list all cities contained in the database
-SELECT cities.id, cities.name, states.name FROM cities JOIN states on states.id = cities.states_id;
+SELECT cities.id, cities.name, states.name FROM cities JOIN states on states.id = cities.state_id;
