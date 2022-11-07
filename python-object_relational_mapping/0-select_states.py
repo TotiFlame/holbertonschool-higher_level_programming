498b57dbb4b9ec60188f7255c22b831de4ada1e0
TotiFlame <santifer4321@gmail.com>
2022-11-07 16:45:25 -0300

    task 0 documented

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
 """ task 0 """
+import MySQLdb
+import sys
 
 
 db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
