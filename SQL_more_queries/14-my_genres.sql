6375f6cca0948325e9780ab27392eb0d41f4693e
TotiFlame <santifer4321@gmail.com>
2022-11-07 13:08:14 -0300

    task 14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -4,4 +4,5 @@ FROM tv_genres
 JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_shows.title = 'Dexter'
-GROUP BY tv_genres.name;
+GROUP BY tv_genres.name
+ORDER BY tv_genres.name ASC;
