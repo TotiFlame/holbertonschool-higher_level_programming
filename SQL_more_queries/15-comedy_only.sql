63ee91f35de5864138948fc3035c7bab96e478e7
TotiFlame <santifer4321@gmail.com>
2022-11-07 13:59:11 -0300

    task 15

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,3 +1,4 @@
+-- Write a script that lists all Comedy shows in the database hbtn_0d_tvshows.
 SELECT ts.title FROM tv_shows ts
 JOIN tv_show_genres tsg ON ts.id = tsg.show_id
 WHERE tsg.genre_id = 5
