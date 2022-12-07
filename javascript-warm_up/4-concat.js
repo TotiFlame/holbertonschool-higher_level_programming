5c07ad8cf7d066faafc28e58fc2cf2d1c5eebed2
TotiFlame <santifer4321@gmail.com>
2022-12-07 15:45:08 -0300

    Task 4 and 5

---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const args = (process.argv.slice(2));
+
+console.log(args[0]);
+console.log('is');
+console.log(args[1]);
---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const args = (process.argv.slice(2));
+
+if (isNaN(args[0])) {
+  console.log('Not a number');
+} else {
+  const argint = parseInt(args[0]);
+  console.log(argint);
+}
