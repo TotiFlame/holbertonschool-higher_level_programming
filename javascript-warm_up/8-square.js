bbbe92e299d7efd178038b6e912e4088f20a62e9
TotiFlame <santifer4321@gmail.com>
2022-12-07 17:05:53 -0300

    task 8 fix

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,16 +1,17 @@
 #!/usr/bin/node
 
-const args = parseInt(process.argv[2]);
+const args = (process.argv.slice(2));
 
-if (!isNaN(args[0])) {
+if (isNaN(args[0])) {
   console.log('Missing size');
 } else {
+  const num = parseInt(args[0]);
   let sq = '';
-  for (let i = 1; i <= args; i++) {
+  for (let i = 1; i <= num; i++) {
     sq += 'X';
   }
   console.log(sq);
-  for (let i = 1; i < args; i++) {
+  for (let i = 1; i < num; i++) {
     console.log(sq);
   }
 }
