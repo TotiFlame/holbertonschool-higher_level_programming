336c36796f0823c7f8c06fc113883ef721fd7967
TotiFlame <santifer4321@gmail.com>
2022-12-08 12:53:37 -0300

    Task 10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -2,10 +2,16 @@
 
 const args = process.argv.slice(2);
 
-if (isNaN(args)) {
-    console.log('1')
+if (isNaN(args[0]) || (!args[0])) {
+  console.log('1');
+} else {
+  factorial(args[0], (args[0] - 1));
 }
 
-function factorial(num) {
-  
+function factorial (num, snum) {
+  if (snum > 1) {
+    num = (num * (snum));
+    return factorial(num, (snum - 1));
+  }
+  console.log(num);
 }
