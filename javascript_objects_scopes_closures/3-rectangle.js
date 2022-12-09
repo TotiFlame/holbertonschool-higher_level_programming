a44a6e43b5ad318f41e2ec5a9f234db93590d619
TotiFlame <santifer4321@gmail.com>
2022-12-09 17:39:34 -0300

    Task 3

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h, width, height) {
+    if ((w !== 0 && w > 0) && (h !== 0 && h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let rec = '';
+    for (let i = 0; i < this.width; i++) {
+      rec += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(rec);
+    }
+  }
+};
